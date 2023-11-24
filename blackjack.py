8d47a4b8eb78a59f808198855def13bad97d800e
ecwong9886 <ecwong9886@gmail.com>
2023-11-24 01:11:54 +0000

    Imported os and time and adjusted spacing to make it look nicer

--- blackjack.py
+++ blackjack.py
@@ -1,4 +1,4 @@
-import random
+import random, time, os 
 
 deck = []
 hands = {}
@@ -26,24 +26,32 @@ def deal_card(person):
     hands[person].append(dealing_card)
     print(f"{dealing_card} was dealt to {person}.")
 
-def print_hands():
-    print()
-    print(f"Dealer's hand: {hands['dealer']}")
-    print(f"Player's hand: {hands['player']}")
+def print_hand(person='both'):
+    if person == 'both':
+        print(f"\nDealer's hand: {hands['dealer']}")
+        print(f"Player's hand: {hands['player']}\n")
+    else:
+        print((f"\nPlayer's hand: {hands[person]}\n"))
 
 def start_game():
+    os.system('cls')
     deck.clear()
     hands["dealer"].clear()
     hands["player"].clear()
     generate_deck()
     print("* * *B L A C K J A C K * * *\n")
+    time.sleep(1)
     deal_card("player")
+    time.sleep(1)
     deal_card("dealer")
+    time.sleep(1)
     deal_card("player")
+    time.sleep(1)
     deal_card("dealer")
-    print_hands()
+    time.sleep(1)
+    print_hand()
 
-def check_naturals(person):
+def check_blackjack(person):
     for card_i in hands[person]:
         if card_i[2] == 1:
             for card_j in hands[person]:
@@ -63,50 +71,83 @@ def get_total(person):
         return total
 
 def player_turn():
-    print("\nPlayer's turn:")
+    time.sleep(1)
+    print("===== Player's turn =====\n")
     while get_total("player") <= 21:
         menu = input("1. Stand\n2. Hit\n")
+        print()
         if menu == "1":
+            time.sleep(1)
+            print("Player stands.")
+            time.sleep(1)
             break
         elif menu == "2":
+            time.sleep(1)
             deal_card("player")
-            print_hands()
+            time.sleep(1)
+            print_hand("player")
+            time.sleep(1)
 
 def dealer_turn():
+    print("\n===== Dealer's turn =====\n")
     while get_total("dealer") < 17:
-        print("Dealer hits.")
+        time.sleep(1)
+        print("Dealer hits.\n")
+        time.sleep(1)
         deal_card("dealer")
+        time.sleep(1)
+        print_hand("dealer")
+    time.sleep(1)
 
+
+print("* * * B L A C K J A C K * * *\n")
+input("Press Enter to start")
 
 while True:
-    input("Press ENTER to start a new game")
+
     start_game()
 
-    if check_naturals("player"):
-        if check_naturals("dealer"):
-            print("Tie.")
+    if check_blackjack("player"):
+        if check_blackjack("dealer"):
+            print("Blackjacks for both player and dealer! Tie.")
         else:
-            print("Player won.")
+            print("Blackjack for player! Player won.")
+        input("\nPress Enter to start a new game...")
+        continue
+    elif check_blackjack("dealer"):
+        print("Blackjack for dealer. Dealer won.")
+        input("\nPress Enter to start a new game...")
         continue
 
     player_turn()
+
     player_total = get_total("player")
     if player_total > 21:
-        print("Player busted!")
+        print("Player busted! Dealer won.")
+        time.sleep(1)
+        input("\nPress Enter to start a new game...")
         continue
 
     dealer_turn()
+
     dealer_total = get_total("dealer")
     if dealer_total > 21:
-        print("Dealer busted!")
+        print("Dealer busted! Player won.")
     else:
         print("Dealer stands.")
+        time.sleep(1)
+        print("\n===== Final results =====")
+        print_hand()
+        time.sleep(1)
         print(f"Player total: {player_total}")
-        print(f"Dealer total: {dealer_total}")
+        print(f"Dealer total: {dealer_total}\n")
+        time.sleep(1)
         if player_total > dealer_total:
             print("Player won.")
         elif dealer_total > player_total:
             print("Dealer won.")
         else:
             print("Tie.")
+    time.sleep(1)
+    input("\nPress Enter to start a new game...")
     continue
